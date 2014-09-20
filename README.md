c3b815894e21a7dbc2be6e8413239abc4b6e4f14
William Bailey <mail@williambailey.org.uk>
2014-09-20 01:21:28 +0100

    Added a few template functions and updated usage and readme.

--- README.md
+++ README.md
@@ -2,6 +2,55 @@
 
 Quick and dirty utility that executes a text templates using data from environment variables.
 
+## Install
+
+```bash
+go get -u github.com/williambailey/go-envtmpl/envtmpl
+```
+
+## Usage
+
+```bash
+envtmpl example example.tmpl
+```
+
+Full usage is available by invoking `envtmpl` without any arguments.
+
+```bash
+envtmpl
+```
+```
+Usage: envtmpl tmplDir tmplName
+
+Parse tmplDir/*.tmpl and renders tmplName to stdout using
+the data from environmental variables.
+
+See http://golang.org/pkg/text/template/ for template syntax.
+
+Exit codes:
+
+0 OK.
+1 Usage.
+2 Template parse error.
+3 Template execution error.
+
+Template Functions:
+
+lower :: Convert to lower case.
+ * {{ "foo BAR bAz" | lower }}
+foo bar baz
+
+title :: Convert to title case.
+ * {{ "foo BAR bAz" | title }}
+Foo BAR BAz
+
+upper :: Convert to upper case.
+ * {{ "foo BAR bAz" | upper }}
+FOO BAR BAZ
+
+exit status 1
+```
+
 ## Contributing
 
 1. Fork the repository on GitHub
--- envtmpl/envtmpl.go
+++ envtmpl/envtmpl.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
-	"text/template"
 	"os"
 	"path/filepath"
 	"strings"
+	"text/template"
 )
 
 const exitOk = 0
 const exitUsage = 1
 const exitTemplateParseError = 2
 const exitTemplateExecutionError = 3
+
+var (
+	funcMap   tmplFuncMap
+	tmplFuncs template.FuncMap
+)
+
+func init() {
+	funcMap = make(tmplFuncMap)
+	initFuncMap()
+	tmplFuncs = make(template.FuncMap)
+	for k, v := range funcMap {
+		tmplFuncs[k] = v.f()
+	}
+}
 
 func main() {
 	os.Exit(realMain())
@@ -30,7 +45,11 @@ func realMain() int {
 	tmplName := args[1]
 	tmplData := make(map[string]string)
 
-	tmpl, err := template.ParseGlob(filepath.Join(tmplDir, "*.tmpl"))
+	tmpl := template.New(
+		fmt.Sprintf("%s [%s]", filepath.Base(os.Args[0]), tmplDir),
+	).Funcs(tmplFuncs)
+
+	_, err := tmpl.ParseGlob(filepath.Join(tmplDir, "*.tmpl"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Template parse error: %s\n", err)
 		return exitTemplateParseError
@@ -53,22 +72,107 @@ func realMain() int {
 }
 
 func usage() {
-	cmd := filepath.Base(os.Args[0])
-	fmt.Fprintf(os.Stderr, strings.TrimSpace(`
-Usage: %s tmplDir tmplName
+	usageTemplate := template.Must(template.New("").Parse(`
+Usage: {{ .cmd }} tmplDir tmplName
 
-  Parse tmplDir/*.tmpl and renders tmplName to stdout using
-  the data from environmental variables.
+Parse tmplDir/*.tmpl and renders tmplName to stdout using
+the data from environmental variables.
 
-  See http://golang.org/pkg/text/template/ for template syntax.
+See http://golang.org/pkg/text/template/ for template syntax.
 
 Exit codes:
 
-  0 OK.
-  1 Usage.
-  2 Template parse error.
-  3 Template execution error.
+0 OK.
+1 Usage.
+2 Template parse error.
+3 Template execution error.
 
-`)+"\n\n", cmd)
+Template Functions:
+
+{{ range $k, $v := .funcs }}{{ $k }} :: {{ $v.Short }}
+{{ range $ex, $out := $v.Example }} * {{ $ex }}
+{{ $out }}
+{{ end }}
+{{ end }}
+
+`))
+	type funcData struct {
+		Short   string
+		Example map[string]string
+	}
+	var u bytes.Buffer
+	funcs := make(map[string]funcData)
+	for n, fn := range funcMap {
+		fd := funcData{
+			Short:   fn.shortUsage(),
+			Example: make(map[string]string),
+		}
+		for _, e := range fn.example(n) {
+			var b bytes.Buffer
+			ex := fmt.Sprintf(e, n)
+			template.Must(
+				template.New(n).Funcs(tmplFuncs).Parse(ex),
+			).Execute(&b, struct{}{})
+			fd.Example[ex] = b.String()
+		}
+		funcs[n] = fd
+	}
+	usageTemplate.Execute(&u, map[string]interface{}{
+		"cmd":   filepath.Base(os.Args[0]),
+		"funcs": funcs,
+	})
+	fmt.Fprintf(os.Stderr, "%s", bytes.TrimSpace(u.Bytes()))
+	fmt.Fprint(os.Stderr, "\n\n")
 	flag.PrintDefaults()
+}
+
+type tmplFuncMap map[string]tmplFunc
+
+type tmplFunc interface {
+	shortUsage() string
+	example(name string) []string
+	f() interface{}
+}
+
+type tmplFuncStruct struct {
+	short    string
+	examples []string
+	fn       interface{}
+}
+
+func (s *tmplFuncStruct) shortUsage() string {
+	return s.short
+}
+
+func (s *tmplFuncStruct) example(name string) []string {
+	return s.examples
+}
+
+func (s *tmplFuncStruct) f() interface{} {
+	return s.fn
+}
+
+func initFuncMap() {
+	const fooExample = `{{ "foo BAR bAz" | %s }}`
+	funcMap["lower"] = &tmplFuncStruct{
+		short: "Convert to lower case.",
+		examples: []string{
+			fooExample,
+		},
+		fn: strings.ToLower,
+	}
+	funcMap["title"] = &tmplFuncStruct{
+		short: "Convert to title case.",
+		examples: []string{
+			fooExample,
+		},
+		fn: strings.Title,
+	}
+	funcMap["upper"] = &tmplFuncStruct{
+		short: "Convert to upper case.",
+		examples: []string{
+			fooExample,
+		},
+		fn: strings.ToUpper,
+	}
 }
